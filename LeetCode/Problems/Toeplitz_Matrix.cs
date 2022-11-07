0fa13ba0af7ffd9cefb9b29986a659c87c3061dd
鄭郡 <003524@i-pass.com.tw>
2022-11-07 15:16:17 +0800

    Add: 766. Toeplitz Matrix

---
+++ LeetCode/Problems/Toeplitz_Matrix.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Problems
+{
+    //766. Toeplitz Matrix 
+    //Easy
+
+    //Given an m x n matrix, return true if the matrix is Toeplitz.Otherwise, return false.
+
+    //A matrix is Toeplitz if every diagonal from top-left to bottom-right has the same elements.
+    public class Toeplitz_Matrix
+    {
+        //Input: matrix = [[1,2,3,4],
+        //                 [5,1,2,3],
+        //                 [9,5,1,2]]
+        //Output: true
+        //Explanation:
+        //            In the above grid, the diagonals are:
+        //"[9]", "[5, 5]", "[1, 1, 1]", "[2, 2, 2]", "[3, 3]", "[4]".
+        //In each diagonal all elements are the same, so the answer is True.
+        public static bool IsToeplitzMatrix(int[][] matrix)
+        {
+            var flag = true;
+            for (var i = 0; i < matrix.Length - 1; i++)
+            {
+                //Console.WriteLine($"$ : {i}");
+                //Console.WriteLine($"matrix[i][0] : {matrix[i][0]}");
+                //if (matrix.Length > i+1 && matrix[i].Length > 1)
+                //{
+                //  Console.WriteLine($"matrix[i + 1][1] : {matrix[i + 1][1]}");
+                //}
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    //Console.WriteLine($"matrix[i][j] : {matrix[i][j]}");
+                    //if (matrix[i].Length > j + 1) {
+                    //  Console.WriteLine($"matrix[i + 1][j + 1] : {matrix[i + 1][j + 1]}");
+                    //}
+                    if (matrix[i].Length > j + 1 && !Equals(matrix[i][j], matrix[i + 1][j + 1]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return flag;
+        }
+    }
+}
--- LeetCode/Program.cs
+++ LeetCode/Program.cs
@@ -1,4 +1,6 @@
 ﻿
 using LeetCode.Problems;
 
-Console.WriteLine(Reverse_Vowels_of_a_String.ReverseVowels("leetcode"));
+
+var matrix = new int[][] { new int[] { 1, 2, 3, 4 }, new int[] { 5, 1, 2, 3 }, new int[] { 9, 5, 1, 2 } };
+Console.WriteLine(Toeplitz_Matrix.IsToeplitzMatrix(matrix));
